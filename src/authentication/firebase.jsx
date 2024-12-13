28958e592e94cc7ddf3419cef7edc6507236fa05
Mukami <78403465+stephmukami@users.noreply.github.com>
2024-12-13 14:34:27 +0300

    Update firebase.jsx

--- src/authentication/firebase.jsx
+++ src/authentication/firebase.jsx
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import dot-env
 import { getAuth,createUserWithEmailAndPassword } from "firebase/auth";
 import {getFirestore,doc,setDoc} from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
@@ -8,16 +9,14 @@ import {getFirestore,doc,setDoc} from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDtcC4G6kkfWDPrhBEud__jompbNbskIh8",
-authDomain: "[twitter-demo-6dd79.firebaseapp.com](http://twitter-demo-6dd79.firebaseapp.com/)",
-projectId: "twitter-demo-6dd79",
-storageBucket: "[twitter-demo-6dd79.appspot.com](http://twitter-demo-6dd79.appspot.com/)",
-messagingSenderId: "882306578442",
-appId: "1:882306578442:web:30b8de93bd5e2134ef07f3",
-measurementId: "G-P43EVBVBRY"
+  apiKey: PROCESS.ENV.VITE_APP_APIKEY,
+authDomain: PROCESS.ENV.VITE_APP_AUTHDOMAIN,
+projectId: PROCESS.ENV.VITE_APP_APIKEY,
+storageBucket: PROCESS.ENV.STORAGEBUCKET,
+messagingSenderId:PROCESS.ENV.VITE_APP_MESSAGING_SENDER_ID,
+appId: PROCESS.ENV.VITE_APP_APP_ID,
+measurementId: PROCESS.ENV.VITE_APP_MEASUREMENT_ID:
 };
-
-
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
